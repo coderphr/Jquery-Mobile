631a7d4bf3e5f84adb01d2f66aa3df9b4bc75c15
coderphr <qq1730684350@163.com>
2020-09-14 15:55:32 +0800

    初始化

---
+++ js/mail.js
@@ -0,0 +1,5 @@
+let listDate = [
+	{"name":"张三","phone":"188888888880","address":"北京市海淀区","img":"img/tel-3.jpg","remark":"同事"},
+	{"name":"李四","phone":"188888888880","address":"北京市海淀区","img":"img/tel-2.jpg","remark":"同事"},
+	{"name":"王五","phone":"188888888880","address":"北京市海淀区","img":"img/tel-1.jpg","remark":"同事"},
+];

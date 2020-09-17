016db5997f50fb373355043fa102a9709def0c70
coderphr <qq1730684350@163.com>
2020-09-17 10:25:30 +0800

    9.15

--- js/mail.js
+++ js/mail.js
@@ -1,5 +1,5 @@
-let listDate = [
-	{"name":"张三","phone":"188888888880","address":"北京市海淀区","img":"img/tel-3.jpg","remark":"同事"},
-	{"name":"李四","phone":"188888888880","address":"北京市海淀区","img":"img/tel-2.jpg","remark":"同事"},
-	{"name":"王五","phone":"188888888880","address":"北京市海淀区","img":"img/tel-1.jpg","remark":"同事"},
-];
+let listData =[
+{"name":"张三", "phone":"1888888088","address":"北京市海定区","img":"img/tel-3.jpg","remark":"同事"},
+{"name":"李四", "phone":"1888888088","address":"北京市海定区","img":"img/tel-2.jpg","remark":"同事"},
+{"name":"王五", "phone":"1888888088","address":"北京市海定区","img":"img/tel-1.jpg","remark":"同事"},
+]
